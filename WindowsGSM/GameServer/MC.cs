d7ec3cacef8e46cc85332305f09b50fc21aba6ec
Felix <f.klinge15@gmail.com>
2020-06-26 18:58:31 +0200

    moved java functions out to JavaHelper class added JavaHelper.cs to Visual Studio project

--- WindowsGSM/GameServer/MC.cs
+++ WindowsGSM/GameServer/MC.cs
@@ -4,9 +4,11 @@ using System.Diagnostics;
 using System.IO;
 using System.Windows;
 using System.Net;
+using System.Threading;
 using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
 using System.Text.RegularExpressions;
+using WindowsGSM.Tools;
 
 namespace WindowsGSM.GameServer
 {
@@ -28,91 +30,6 @@ namespace WindowsGSM.GameServer
         public string Defaultmap = "world";
         public string Maxplayers = "20";
         public string Additional = "-Xmx1024M -Xms1024M";
-
-        private static bool isJavaInPath()
-        {
-            ProcessStartInfo psi = new ProcessStartInfo("cmd.exe");
-            psi.RedirectStandardOutput = true;
-            psi.RedirectStandardError = true;
-            psi.UseShellExecute = false;
-            psi.CreateNoWindow = true;
-            psi.Arguments = "/c java -version";
-            Process p = Process.Start(psi);
-            string output = p.StandardOutput.ReadToEnd();
-            string error = p.StandardError.ReadToEnd();
-
-            return (p.ExitCode == 0);
-        }
-
-        private static string findNewestJavaRuntimeInDirectory(string javaDirectory)
-        {
-            if (!Directory.Exists(javaDirectory))
-            {
-                return string.Empty;
-            }
-
-            List<string> javaDirectories = new List<string>(Directory.EnumerateDirectories(javaDirectory));
-            if (javaDirectories.Count == 0)
-            {
-                return string.Empty;
-            }
-
-            javaDirectories.Sort();
-            return javaDirectories[0];
-        }
-
-        private static string findJavaExecutableInJavaRuntimeDirectory(string javaRuntimeAbsolutePath)
-        {
-            string javaExecutableAbsolutePath = Path.Combine(javaRuntimeAbsolutePath, "java.exe");
-            if( File.Exists(javaExecutableAbsolutePath))
-            {
-                return javaExecutableAbsolutePath;
-            }
-            
-            javaExecutableAbsolutePath = Path.Combine(javaRuntimeAbsolutePath, "bin", "java.exe");
-            return javaExecutableAbsolutePath;
-        }
-
-        private static string findNewestJavaExecutable()
-        {
-            if (isJavaInPath())
-            {
-                //assume that the newest version is part of $PATH
-                return "java.exe";
-            }
-
-            string javaDirectoryAbsolutePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "Java");
-            string javaRuntimeAbsolutePath = findNewestJavaRuntimeInDirectory(javaDirectoryAbsolutePath);
-
-            if (javaRuntimeAbsolutePath.Length == 0)
-            {
-                if(!Environment.Is64BitOperatingSystem)
-                {
-                    return string.Empty;
-                }
-
-                string javaDirectoryAbsolutePathX86 = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "Java");
-                javaRuntimeAbsolutePath = findNewestJavaRuntimeInDirectory(javaDirectoryAbsolutePathX86);
-            }
-
-            return findJavaExecutableInJavaRuntimeDirectory(javaRuntimeAbsolutePath);
-        }
-
-        private enum Java : int
-        {
-            NotInstall = 0,
-            InstalledGlobal = 1, //(java)
-            InstalledAbsolute = 2 //Path: (C:\Program Files (x86)\Java\jre1.8.0_231\bin\java.exe)
-        }
-
-        private static string[] _JavaData =
-        {
-            $"jre-8u241-windows-{(Environment.Is64BitOperatingSystem ? "x64" : "i586")}.exe",
-            Environment.Is64BitOperatingSystem ?
-                "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=241536_1f5b5a70bf22433b84d0e960903adac8" :
-                "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=241534_1f5b5a70bf22433b84d0e960903adac8",
-            findNewestJavaExecutable()
-        };
 
         public MC(Functions.ServerConfig serverData)
         {
@@ -139,8 +56,8 @@ namespace WindowsGSM.GameServer
 
         public async Task<Process> Start()
         {
-            Java isJavaInstalled = IsJavaJREInstalled();
-            if (isJavaInstalled == Java.NotInstall)
+            string javaPath = JavaHelper.FindJavaExecutableAbsolutePath();
+            if (javaPath.Length == 0)
             {
                 Error = "Java is not installed";
                 return null;
@@ -161,15 +78,10 @@ namespace WindowsGSM.GameServer
                 Notice = $"server.properties not found ({configPath}). Generated a new one.";
             }
 
-            string javaPath = (isJavaInstalled == Java.InstalledGlobal) ? "java" : _JavaData[2];
-
-            if (isJavaInstalled == Java.InstalledAbsolute)
+            WindowsFirewall firewall = new WindowsFirewall("java.exe", javaPath);
+            if (!await firewall.IsRuleExist())
             {
-                WindowsFirewall firewall = new WindowsFirewall("java.exe", javaPath);
-                if (!await firewall.IsRuleExist())
-                {
-                    firewall.AddRule();
-                }
+                firewall.AddRule();
             }
 
             Process p;
@@ -244,7 +156,7 @@ namespace WindowsGSM.GameServer
             }
 
             //Install JAVA if not installed
-            if (IsJavaJREInstalled() == 0)
+            if (!JavaHelper.IsJREInstalled())
             {
                 //Java
                 result = MessageBox.Show("Java is not installed\n\nWould you like to install?", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
@@ -254,8 +166,10 @@ namespace WindowsGSM.GameServer
                     return null;
                 }
 
-                if (!await DownloadJavaJRE())
+                JavaHelper.JREDownloadTaskResult taskResult = await JavaHelper.DownloadJREToServer(_serverData.ServerID);
+                if (!taskResult.installed)
                 {
+                    Error = taskResult.error;
                     return null;
                 }
             }
@@ -316,10 +230,12 @@ namespace WindowsGSM.GameServer
         public async Task<bool> Update()
         {
             //Install JAVA if not installed
-            if (IsJavaJREInstalled() == Java.NotInstall)
+            if (!JavaHelper.IsJREInstalled())
             {
-                if (!await DownloadJavaJRE())
+                JavaHelper.JREDownloadTaskResult taskResult = await JavaHelper.DownloadJREToServer(_serverData.ServerID);
+                if (!taskResult.installed)
                 {
+                    Error = taskResult.error;
                     return false;
                 }
             }
@@ -464,86 +380,6 @@ namespace WindowsGSM.GameServer
 
             Error = $"Fail to get remote build";
             return string.Empty;
-        }
-
-        private static Java IsJavaJREInstalled()
-        {
-            try
-            {
-                if (isJavaInPath())
-                {
-                    return Java.InstalledGlobal;
-                }
-            }
-            catch
-            {
-                //ignore
-            }
-
-            try
-            {
-                ProcessStartInfo psi = new ProcessStartInfo("cmd.exe");
-                psi.RedirectStandardOutput = true;
-                psi.RedirectStandardError = true;
-                psi.UseShellExecute = false;
-                psi.CreateNoWindow = true;
-                psi.Arguments = $"/c \"{_JavaData[2]}\" -version";
-                Process p = Process.Start(psi);
-                string output = p.StandardOutput.ReadToEnd();
-                string error = p.StandardError.ReadToEnd();
-
-                if (p.ExitCode == 0)
-                {
-                    return Java.InstalledAbsolute;
-                }
-            }
-            catch
-            {
-                //ignore
-            }
-
-            return Java.NotInstall;
-        }
-
-        private async Task<bool> DownloadJavaJRE()
-        {
-            string serverFilesPath = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID);
-            string filename = _JavaData[0];
-            string installLink = _JavaData[1];
-
-            //Download jre-8u231-windows-i586-iftw.exe from https://www.java.com/en/download/manual.jsp
-            string jrePath = Path.Combine(serverFilesPath, filename);
-            try
-            {
-                using (WebClient webClient = new WebClient())
-                {
-                    //Run jre-8u231-windows-i586-iftw.exe to install Java
-                    await webClient.DownloadFileTaskAsync(installLink, jrePath);
-                    string installPath = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID);
-                    ProcessStartInfo psi = new ProcessStartInfo(jrePath);
-                    psi.WorkingDirectory = installPath;
-                    psi.Arguments = $"INSTALL_SILENT=Enable INSTALLDIR=\"{_JavaData[3]}\"";
-                    Process p = new Process
-                    {
-                        StartInfo = psi,
-                        EnableRaisingEvents = true
-                    };
-                    p.Start();
-
-                    while (!File.Exists(_JavaData[2]))
-                    {
-                        await Task.Delay(100);
-                        continue;
-                    }
-                }
-            }
-            catch
-            {
-                Error = "Fail to download " + filename;
-                return false;
-            }
-
-            return true;
         }
     }
 }
