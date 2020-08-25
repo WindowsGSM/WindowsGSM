404dcc3b48f54eacc98c84bed1f91e499eedcd01
Stian Tofte <stian@ohnee.com>
2020-08-25 03:22:51 +0200

    ASTRONEER Plugin

---
+++ WindowsGSM-Plugin-Development/Plugins/ASTRONEER.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsGSM.Functions;
+using WindowsGSM.GameServer.Query;
+using WindowsGSM.GameServer.Engine;
+using System.Net;
+
+namespace WindowsGSM.Plugins
+{
+    public class ASTRONEER : SteamCMDAgent
+    {
+        // - Plugin Details
+        public Plugin Plugin = new Plugin
+        {
+            name = "WindowsGSM.ASTRONEER", // WindowsGSM.XXXX
+            author = "1stian",
+            description = "🧩 WindowsGSM plugin for supporting Astroneer Dedicated Server",
+            version = "1.0",
+            url = "https://github.com/1stian/WindowsGSM.ASTRONEER", // Github repository link (Best practice)
+            color = "#34c9eb" // Color Hex
+        };
+
+
+        // - Standard Constructor and properties
+        public ASTRONEER(ServerConfig serverData) : base(serverData) => base.serverData = _serverData = serverData;
+        private readonly ServerConfig _serverData;
+        public string Error, Notice;
+
+        // - Settings properties for SteamCMD installer
+        public override bool loginAnonymous => true; // ASTRONEER requires to login steam account to install the server, so loginAnonymous = false
+        public override string AppId => "728470"; // Game server appId, ASTRONEER is 728470
+
+
+        // - Game server Fixed variables
+        public string StartPath => "AstroServer.exe"; // Game server start path
+        public string FullName = "Astroneer Dedicated Server"; // Game server FullName
+        public bool AllowsEmbedConsole = false;  // Does this server support output redirect?
+        public int PortIncrements = 1; // This tells WindowsGSM how many ports should skip after installation
+        public object QueryMethod = new A2S(); // Query method should be use on current server type. Accepted value: null or new A2S() or new FIVEM() or new UT3()
+
+
+        // - Game server default values
+        public string Port = "7777"; // Default port
+        public string QueryPort = "7777"; // Default query port
+        public string Defaultmap = "empty"; // Default map name
+        public string Maxplayers = "4"; // Default maxplayers
+        public string Additional = ""; // Additional server start parameter
+
+
+        // - Create a default cfg for the game server after installation
+        public async void CreateServerCFG() { }
+
+
+        // - Start server function, return its Process to WindowsGSM
+        public async Task<Process> Start()
+        {
+            // Prepare start parameter
+            var param = new StringBuilder();
+            param.Append(string.IsNullOrWhiteSpace(_serverData.ServerParam) ? string.Empty : $" {_serverData.ServerParam}");
+
+            QueryPort = Port;
+
+            string workingDir = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID);
+            string runPath = Path.Combine(workingDir, "Astro\\Binaries\\Win64\\AstroServer-Win64-Shipping.exe");
+
+            // Prepare Process
+            var p = new Process
+            {
+                StartInfo =
+                {
+                    WindowStyle = ProcessWindowStyle.Minimized,
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    WorkingDirectory = ServerPath.GetServersServerFiles(_serverData.ServerID),
+                    FileName = runPath,
+                    Arguments = param.ToString()
+                },
+                EnableRaisingEvents = true
+            };
+
+            // Start Process
+            try
+            {
+                p.Start();
+                return p;
+            } catch (Exception e)
+            {
+                base.Error = e.Message;
+                return null; // return null if fail to start
+            }
+        }
+
+
+        // - Stop server function
+        public async Task Stop(Process p) => await Task.Run(() => { p.Kill(); });
+    }
+}
