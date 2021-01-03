93487e99447be65fd1d81b7c774a36b194fe9d2b
Battlefield Duck <hkdsethomas@gmail.com>
2021-01-03 11:38:55 +0800

    Update Bot.cs

--- WindowsGSM/DiscordBot/Bot.cs
+++ WindowsGSM/DiscordBot/Bot.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using Discord;
-using Discord.Commands;
 using Discord.Rest;
 using Discord.WebSocket;
-using WindowsGSM.Functions;
 
 namespace WindowsGSM.DiscordBot
 {
